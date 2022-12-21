a1f4328dd5eaec88b1614da6af0c1649ed1ee078
akyl <akylbekbektenaliev@gmail.com>
2022-12-21 10:27:21 +0600

    Structure changes

--- src/main/java/com/spectro/spectro/repository/LaptopRepo.java
+++ src/main/java/com/spectro/spectro/repository/LaptopRepo.java
@@ -1,63 +1,11 @@
 package com.spectro.spectro.repository;
 
 import com.spectro.spectro.entity.LaptopEntity;
-import com.sun.xml.bind.v2.model.core.ID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface LaptopRepo extends JpaRepository<LaptopEntity, Long>, JpaSpecificationExecutor<LaptopEntity> {
-
-    LaptopEntity findById(ID id);
-
     LaptopEntity findByModel(String model);
-
-    LaptopEntity findByManufacturer(String manufature);
-
-    LaptopEntity findByCamera(String camera);
-
-    LaptopEntity findByOS(String os);
-
-    LaptopEntity findByHousingMaterial(String housingMaterial);
-
-    LaptopEntity findByProcessorType(String processorType);
-
-    LaptopEntity findByRAM(String RAM);
-
-    LaptopEntity findByMemory(String memory);
-
-    LaptopEntity findByScreenSize(String screenSize);
-
-    LaptopEntity findByScreenResolution(String screenResolution);
-
-    LaptopEntity findByHeadphoneJack(String headphone_jack);
-
-    LaptopEntity findByVideoCard(String video_card);
-
-    List<LaptopEntity> findAllByModel(String model);
-
-    List<LaptopEntity> findAllByManufacturer(String manufature);
-
-    List<LaptopEntity> findAllByCamera(String camera);
-
-    List<LaptopEntity> findAllByOS(String os);
-
-    List<LaptopEntity> findAllByHousingMaterial(String housingMaterial);
-
-    List<LaptopEntity> findAllByProcessorType(String processorType);
-
-    List<LaptopEntity> findAllByRAM(String RAM);
-
-    List<LaptopEntity> findAllByMemory(String memory);
-
-    List<LaptopEntity> findAllByScreenSize(String screenSize);
-
-    List<LaptopEntity> findAllByScreenResolution(String screenResolution);
-
-    List<LaptopEntity> findAllByHeadphoneJack(String headphone_jack);
-
-    List<LaptopEntity> findAllByVideoCard(String video_card);
 }
--- src/main/java/com/spectro/spectro/service/LaptopService.java
+++ src/main/java/com/spectro/spectro/service/LaptopService.java
@@ -20,33 +20,38 @@ public class LaptopService {
     private LaptopCriteriaRepo laptopCriteriaRepo;
 
     public void save(LaptopEntity laptop){
-        if(laptopRepo.findByModel(laptop.getModel())==null)this.laptopRepo.save(laptop);
+        if(laptopRepo.findByModel(laptop.getModel())==null){
+            if(laptop.getAmount()>0){
+                laptop.setStatus(LaptopEnum.available);
+            }else laptop.setStatus(LaptopEnum.deleted);
+            this.laptopRepo.save(laptop);
+        }
     }
 
     public void update(LaptopEntity laptop) throws UserNotFoundException {
         LaptopEntity laptop1 = laptopRepo.findByModel(laptop.getModel());
         if(laptopRepo.findByModel(laptop.getModel())!=null){
-            laptop1.setAmount(laptop.getAmount());
-            laptop1.setCamera(laptop.getCamera());
-            laptop1.setConnectors(laptop.getConnectors());
-            laptop1.setDescription(laptop.getDescription());
-            laptop1.setDimensions(laptop.getDimensions());
-            laptop1.setHeadphoneJack(laptop.getHeadphoneJack());
-            laptop1.setHousingMaterial(laptop.getHousingMaterial());
-            laptop1.setMemory(laptop.getMemory());
-            laptop1.setManufacturer(laptop.getManufacturer());
-            laptop1.setModel(laptop.getModel());
-            laptop1.setNumberOfCores(laptop.getNumberOfCores());
-            laptop1.setOS(laptop.getOS());
-            laptop1.setPrice(laptop.getPrice());
-            laptop1.setProcessorType(laptop.getProcessorType());
-            laptop1.setProducingCountry(laptop.getProducingCountry());
-            laptop1.setRAM(laptop.getRAM());
-            laptop1.setSpeaker(laptop.getSpeaker());
-            laptop1.setScreenSize(laptop.getScreenSize());
-            laptop1.setScreenResolution(laptop.getScreenResolution());
-            laptop1.setVideoCard(laptop.getVideoCard());
-            laptop1.setStatus(laptop.getStatus());
+            if (laptop.getAmount()>=0)laptop1.setAmount(laptop.getAmount());
+            if(laptop.getCamera()!=null)laptop1.setCamera(laptop.getCamera());
+            if(laptop.getConnectors()!=null)laptop1.setConnectors(laptop.getConnectors());
+            if(laptop.getDescription()!=null)laptop1.setDescription(laptop.getDescription());
+            if(laptop.getDimensions()!=null)laptop1.setDimensions(laptop.getDimensions());
+            if(laptop.getHeadphoneJack()!=null)laptop1.setHeadphoneJack(laptop.getHeadphoneJack());
+            if(laptop.getHousingMaterial()!=null)laptop1.setHousingMaterial(laptop.getHousingMaterial());
+            if(laptop.getMemory()!=null)laptop1.setMemory(laptop.getMemory());
+            if(laptop.getManufacturer()!=null)laptop1.setManufacturer(laptop.getManufacturer());
+            if(laptop.getModel()!=null)laptop1.setModel(laptop.getModel());
+            if(laptop.getNumberOfCores()!=null)laptop1.setNumberOfCores(laptop.getNumberOfCores());
+            if(laptop.getOS()!=null)laptop1.setOS(laptop.getOS());
+            if(laptop.getPrice()!=null)laptop1.setPrice(laptop.getPrice());
+            if(laptop.getProcessorType()!=null)laptop1.setProcessorType(laptop.getProcessorType());
+            if(laptop.getProducingCountry()!=null)laptop1.setProducingCountry(laptop.getProducingCountry());
+            if(laptop.getRAM()!=null)laptop1.setRAM(laptop.getRAM());
+            if(laptop.getSpeaker()!=null)laptop1.setSpeaker(laptop.getSpeaker());
+            if(laptop.getScreenSize()!=null)laptop1.setScreenSize(laptop.getScreenSize());
+            if(laptop.getScreenResolution()!=null)laptop1.setScreenResolution(laptop.getScreenResolution());
+            if(laptop.getVideoCard()!=null)laptop1.setVideoCard(laptop.getVideoCard());
+            if(laptop.getStatus()!=null)laptop1.setStatus(laptop.getStatus());
             laptopRepo.save(laptop1);
         }else{
             throw new UserNotFoundException("Can not update laptop list. It doesn't exist");
