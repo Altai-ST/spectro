92f2e03363dca525577a2588ce3805210b8fc25f
akyl <akylbekbektenaliev@gmail.com>
2022-12-23 04:30:41 +0600

    basket test

--- src/main/java/com/spectro/spectro/repository/DetailorderedRepo.java
+++
@@ -1,9 +0,0 @@
-package com.spectro.spectro.repository;
-
-import com.spectro.spectro.entity.DetailorderedEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DetailorderedRepo extends CrudRepository<DetailorderedEntity, Long> {
-    DetailorderedEntity findByDetail(String detail);
-    DetailorderedEntity findByAmount(String amount);
-}
